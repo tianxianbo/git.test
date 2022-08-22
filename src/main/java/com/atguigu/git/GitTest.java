60eb0e0280f0b072bfc023267d6588a1c144b195
Tian <Tian@163.com>
2022-08-22 22:01:56 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello git!4");
         System.out.println("master test");
         System.out.println("hot-fix test1");
+        System.out.println("push test2");
     }
 }
