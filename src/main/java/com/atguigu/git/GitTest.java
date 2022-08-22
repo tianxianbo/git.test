1969d8c94dc3d1a734d4704f7d880e159f7740c2
Tian <Tian@163.com>
2022-08-22 20:32:20 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author txb
+ * @create 2022-08-22 20:28
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
